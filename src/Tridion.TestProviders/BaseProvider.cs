76a629014621e2262b7707eb1f8f72e66164fe3f
quirijnslings <quirijn.slings@trivident.com>
2020-03-28 13:44:23 +0100

    renamed Web.config to Web.config.pp

--- src/Tridion.TestProviders/BaseProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using DD4T.ContentModel.Contracts.Providers;
-
-namespace Tridion.TestProviders
-{
-    public class BaseProvider : IProvider
-    {
-        public int PublicationId { get; set; }
-    }
-}
