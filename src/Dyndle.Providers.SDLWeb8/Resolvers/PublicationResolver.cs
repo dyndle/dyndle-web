926510ed2f53815b6f7411ad076f1228acf2fbbd
Erik <erik.henderiks@trivident.com>
2020-03-30 12:17:14 +0200

    Rename Dyndle.Modules to Dyndle.Providers for sdlweb packages.

---
+++ src/Dyndle.Providers.SDLWeb8/Resolvers/PublicationResolver.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Web;
+using DD4T.ContentModel.Contracts.Logging;
+using Dyndle.Modules.Core;
+using Dyndle.Modules.Core.Configuration;
+using Dyndle.Modules.Core.Contracts;
+using Dyndle.Modules.Core.Extensions;
+using Dyndle.Modules.Core.Resolvers;
+using Tridion.ContentDelivery.DynamicContent;
+
+namespace Dyndle.Providers.SDLWeb8.Resolvers
+{
+    /// <summary>
+    /// Resolve publication id by querying Tridion DiscoveryService;
+    /// </summary>
+    /// <seealso cref="IExtendedPublicationResolver" />
+    public class PublicationResolver : PublicationResolverBase
+    {
+        private readonly DynamicMappingsRetriever _mappingsRetriever;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PublicationResolver" /> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        public PublicationResolver(ILogger logger) : base(logger)
+        {
+            _mappingsRetriever = new DynamicMappingsRetriever();
+        }
+
+        protected override PublicationMapping RetrieveMapping(string url)
+        {
+            return ToNativePublicationMapping(_mappingsRetriever.GetPublicationMapping(url));
+        }
+
+        private PublicationMapping ToNativePublicationMapping(IPublicationMapping result)
+        {
+            if (result == null)
+            {
+                return null;
+            }
+            return new PublicationMapping()
+            {
+                Domain = result.Domain,
+                PublicationId = result.PublicationId,
+                Protocol = result.Protocol,
+                Port = result.Port,
+                Path = result.Path,
+                PathScanDepth = result.PathScanDepth
+            };
+        }
+    }
+}
