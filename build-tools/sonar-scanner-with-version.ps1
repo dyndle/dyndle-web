df8218737451df41776f89896a86104a57365330
Kunal Shetye <kunal.shetye@trivident.com>
2020-04-02 15:04:18 +0200

    uncommented batch file execution

--- build-tools/sonar-scanner-with-version.ps1
+++ build-tools/sonar-scanner-with-version.ps1
@@ -2,4 +2,4 @@ $gitbranch = git rev-parse --abbrev-ref HEAD
 $whoami = whoami
 $env:SonarScannerVersion = $env:username+":"+$gitbranch
 Write-Host $env:SonarScannerVersion
-#cmd /c "sonar-scanner.bat"
+cmd /c "sonar-scanner.bat"
