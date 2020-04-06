3675c11980bffd5a35ed94dbeb03a9d5c1c9b11f
Praedo <praedo4421@gmail.com>
2020-04-06 15:57:08 +0200

    Split QueryCriteria file into multiple files and moved them into Query folder

---
+++ src/Dyndle.Modules.Core/Models/Query/SortColumn.cs
@@ -0,0 +1,73 @@
+namespace Dyndle.Modules.Core.Models.Query
+{
+    /// <summary>
+    /// Enum SortColumn
+    /// </summary>
+    public enum SortColumn
+    {
+        /// <summary>
+        /// The item reference identifier
+        /// </summary>
+        ItemReferenceId,
+        /// <summary>
+        /// The item URL
+        /// </summary>
+        ItemUrl,
+        /// <summary>
+        /// The item page template identifier
+        /// </summary>
+        ItemPageTemplateId,
+        /// <summary>
+        /// The item last published date
+        /// </summary>
+        ItemSchemaId,
+        /// <summary>
+        /// The item file name
+        /// </summary>
+        ItemFileName,
+        /// <summary>
+        /// The item modification date
+        /// </summary>
+        ItemModificationDate,
+        /// <summary>
+        /// The item last published date
+        /// </summary>
+        ItemLastPublishedDate,
+        /// <summary>
+        /// The item trustee
+        /// </summary>
+        ItemTrustee,
+        /// <summary>
+        /// The item creation date
+        /// </summary>
+        ItemCreationDate,
+        /// <summary>
+        /// The item title
+        /// </summary>
+        ItemTitle,
+        /// <summary>
+        /// The item item type
+        /// </summary>
+        ItemItemType,
+        /// <summary>
+        /// The item owning publication identifier
+        /// </summary>
+        ItemOwningPublicationId,
+        /// <summary>
+        /// The item minor version
+        /// </summary>
+        ItemMinorVersion,
+        /// <summary>
+        /// The item major version
+        /// </summary>
+        ItemMajorVersion,
+        /// <summary>
+        /// The item publication identifier
+        /// </summary>
+        ItemPublicationId,
+        /// <summary>
+        /// The item initial publication date
+        /// </summary>
+        ItemInitialPublicationDate
+    }
+}
