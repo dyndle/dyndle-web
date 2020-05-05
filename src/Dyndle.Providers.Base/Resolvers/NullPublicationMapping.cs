44a06f3d3d816baeac2be2d0653a0248cb010fda
quirijnslings <quirijn.slings@trivident.com>
2020-05-05 15:36:42 +0200

    fixed bug in publication resolver

---
+++ src/Dyndle.Providers.Base/Resolvers/NullPublicationMapping.cs
@@ -0,0 +1,19 @@
+﻿using Tridion.ContentDelivery.DynamicContent;
+
+namespace Dyndle.Providers.Resolvers
+{
+    public class NullPublicationMapping : IPublicationMapping
+    {
+        public int PublicationId => 0;
+
+        public string Protocol => null;
+
+        public string Domain => null;
+
+        public string Port => null;
+
+        public string Path => null;
+
+        public int PathScanDepth => 0;
+    }
+}
